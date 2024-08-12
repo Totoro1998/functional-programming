e65bb6c2825c98856d263781cb58c9aa7237a891
totoro <totoro.fu@webeye.com>
2024-08-12 11:27:27 +0800

    rxjs优化

---
+++ rxjs/1.初识rxjs.js
@@ -0,0 +1,28 @@
+import { Observable } from "rxjs";
+
+// 创建一个可观察对象
+const observable = new Observable((subscriber) => {
+  subscriber.next(1);
+  subscriber.next(2);
+  subscriber.next(3);
+  setTimeout(() => {
+    subscriber.next(4);
+    subscriber.complete();
+  }, 1000);
+});
+
+console.log("just before subscribe");
+
+// 当可观察对象被订阅时，立刻Push推送值
+observable.subscribe({
+  next(x) {
+    console.log("got value " + x);
+  },
+  error(err) {
+    console.error("something wrong occurred: " + err);
+  },
+  complete() {
+    console.log("done");
+  },
+});
+console.log("just after subscribe");
